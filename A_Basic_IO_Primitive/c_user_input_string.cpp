5fac4f6714cd1afe4572646811db83fea7914afe
mallabiju <mallabiju@gmail.com>
2023-08-07 23:13:14 +0545

    User input string

---
+++ A_Basic_IO_Primitive/c_user_input_string.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string fname;
+    cout << "Enter your first name:";
+    cin >> fname;
+    cout << "Your first name =" << fname;
+    return 0;
+}
