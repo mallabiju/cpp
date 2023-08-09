a978a66a0509c442b4b068e234ab975c16c4b06b
mallabiju <mallabiju@gmail.com>
2023-08-09 19:45:37 +0545

    Working with class and object

---
+++ A_Basic_IO_Primitive/i_set_width.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+    cout << setw(50) << "Hello Nepal" << endl;
+    cout << setw(100) << "Hello Sanothimi" << endl;
+    return 0;
+}
---
+++ B_Basic_IO_Class_n_Object/a_hello_nepal.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+class Student{
+    public:
+        string str = "Hello Nepal";
+};
+
+int main()
+{
+    Student s1;
+    cout << s1.str;
+    return 0;
+}
