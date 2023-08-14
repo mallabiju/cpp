e3800d1acdcf1e6cc75291d343d0a88d9ac8c3ef
mallabiju <mallabiju@gmail.com>
2023-08-14 09:36:48 +0545

    New updates

---
+++ B_Basic_IO_Class_n_Object/k_sum_diff_pro_div.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+class Calculator
+{
+    int a, b;
+
+public:
+    void input()
+    {
+        cout << "Enter first number: ";
+        cin >> a;
+        cout << "Enter second number: ";
+        cin >> b;
+    }
+    void add()
+    {
+        cout << "The sum of " << a << " and " << b << " is = " << a + b << endl;
+    }
+    void diff()
+    {
+        cout << "The difference of " << a << " and " << b << " is = " << a - b << endl;
+    }
+    void pro()
+    {
+        cout << "The product of " << a << " and " << b << " is = " << a * b << endl;
+    }
+    void div()
+    {
+        cout << "Quotient while dividing " << a << " by " << b << " is = " << a / b;
+    }
+};
+
+int main()
+{
+    Calculator c;
+    c.input();
+    c.add();
+    c.diff();
+    c.pro();
+    c.div();
+    return 0;
+}
