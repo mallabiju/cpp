8dc54f4d0cf3ab80244748ca4e2e37db5e78de86
mallabiju <mallabiju@gmail.com>
2023-08-28 11:04:42 +0545

    Loop and Switch

---
+++ C_Control_Statement/UsingPoP/b_loop/c_for.cpp
@@ -0,0 +1,16 @@
+// Syntax
+// ------
+// for(initialization; condition; increment/ decrement){
+//     statement to print
+// }
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int i = 0; i < 10; i++)
+    {
+        cout << i << endl;
+    }
+}
